f20f6fd8c6793ba9851c9717d8a4ea809e3a17d8
KatUser <ekaterina.shirokikh@gmail.com>
2023-01-28 16:28:07 +0700

    Правила Checkstyle: private for all fields, public for methods & constructor

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,26 +1,26 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    int sizeOfEmpty = 10;
-    String name;
-    public String surname;
-    public static final String NEW_VALUE = "";
+    private final int sizeOfEmpty = 10;
+    private String name;
+    private String surname;
+    private static final String NEW_VALUE = "";
 
-    Broken() {
-
-    }
-
-    void echo() {
+    public Broken() {
 
     }
 
-    void media(Object obj) {
+    public void echo() {
+
+    }
+
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c) {
+    public void method(int a, int b, int c) {
 
     }
 }
