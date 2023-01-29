4b084a61c809d37f5112e64ae80ee3e2c1cd88ac
KatUser <ekaterina.shirokikh@gmail.com>
2023-01-29 10:22:40 +0700

    1.3. Вызов метода с аргументами - with switch-master instead of if-else

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -2,13 +2,12 @@ package ru.job4j.oop;
 
 public class Jukebox {
     public void music(int position) {
-        if (position == 1) {
-            System.out.println("Пусть бегут неуклюже");
-        } else if (position == 2) {
-            System.out.println("Спокойной ночи");
-        } else {
-            System.out.println("Песня не найдена");
-        }
+        String str = switch (position) {
+            case 1 -> "Пусть бегут неуклюже";
+            case 2 -> "Спокойной ночи";
+            default -> "Песня не найдена";
+        };
+        System.out.println(str);
     }
 
     public static void main(String[] args) {
@@ -16,6 +15,5 @@ public class Jukebox {
         musixBox.music(1);
         musixBox.music(2);
         musixBox.music(0);
-
     }
 }
