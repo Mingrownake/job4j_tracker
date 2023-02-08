d920337ea1257790fe8a7a2f2190a5661d383d02
KatUser <ekaterina.shirokikh@gmail.com>
2023-02-08 18:55:26 +0700

    5. Tracker - хранилище, added: findByName, findAll

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +24,35 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] addedAllItems = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item itemToAdd = items[index];
+            if (itemToAdd != null) {
+                addedAllItems[index] = itemToAdd;
+                size++;
+            }
+        }
+        addedAllItems = Arrays.copyOf(addedAllItems, size);
+        return addedAllItems;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] addedItemsByName = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item itemToAdd = items[index];
+            if (itemToAdd != null) {
+                if (key.equals(itemToAdd.getName())) {
+                    addedItemsByName[index] = itemToAdd;
+                    size++;
+                }
+            }
+        }
+        addedItemsByName = Arrays.copyOf(addedItemsByName, size);
+        return addedItemsByName;
     }
 }
