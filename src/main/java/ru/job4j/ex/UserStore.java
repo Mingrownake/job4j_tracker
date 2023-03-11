e785543f994e5dc44828695c6daccbfa73eac87f
KatUser <ekaterina.shirokikh@gmail.com>
2023-03-11 11:44:01 +0700

    0.5. Иерархия исключений и множественный catch. [#219368] User

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,38 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                return user;
+            } else {
+                throw new UserNotFoundException("The user "
+                        + login + " was not found.");
+            }
+        }
+        return null;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user.getUsername().length() >= 3 && user.isValid()) {
+            return true;
+        } else {
+            throw new UserInvalidException("The user "
+                    + user.getUsername() + " is not valid.");
+        }
+    }
+
+    public static void main(String[] args) throws UserNotFoundException {
+        User[] users = {
+                new User("Qwe", false)
+        };
+        User user = findUser(users, "Qwe");
+        try {
+            if (validate(user)) {
+                System.out.println("This user has the access.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
