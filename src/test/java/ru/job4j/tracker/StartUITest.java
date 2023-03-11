720b61301c5e93234501704d760d9f0364946c14
KatUser <ekaterina.shirokikh@gmail.com>
2023-03-11 12:43:45 +0700

    1.1. Тесты на StartUI. [#223313] Exit the App test

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -181,4 +181,26 @@ public class StartUITest {
                         + "=== Exit Program ===" + ln
         );
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[] {
+                new ExitAppAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo(
+                "Menu." + ln
+                                + "0. Exit the App." + ln
+                                + "Wrong input, you can select: from 0 to 0." + ln
+                                + "Menu." + ln
+                                + "0. Exit the App." + ln
+                                + "=== Exit Program ===" + ln
+        );
+    }
 }
