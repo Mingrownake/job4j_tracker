c842151c4ef34d038d50fbd4baa978e4ca0e60cd
KatUser <ekaterina.shirokikh@gmail.com>
2023-03-26 15:14:31 +0700

    3. Автоупаковка / автораспаковка [#504971]

---
+++ src/main/java/ru/job4j/collection/Box.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+public class Box {
+    public static void main(String[] args) {
+        int a = 1;
+        Integer b; /*значение примитивного типа int присваивается типу Integer:; b = Integer.valueOf(a); */
+        b = a;
+        System.out.println("Integer b = " + b);
+        int x = 2;
+        num(x);
+    }
+
+    public static void num(Integer x) {
+        System.out.println("Значение типа Integer: " + x);
+    }
+
+    public static void num(int x) {
+        System.out.println("Значение типа int: " + x);
+    }
+}
