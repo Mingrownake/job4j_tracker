07446de6dd11979baa9f843d71b7e17483d5ebbf
KatUser <ekaterina.shirokikh@gmail.com>
2023-04-16 15:45:01 +0700

    0. Set, HashSet, Iterator. [#10095] - UsageHashSet with duplicate autos

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -6,9 +6,15 @@ public class UsageHashSet {
     public static void main(String[] args) {
         HashSet<String> autos = new HashSet<>();
         autos.add("Lada");
+        autos.add("Kalina");
+        autos.add("Kalina");
         autos.add("BMW");
         autos.add("Volvo");
+        autos.add("Volvo");
         autos.add("Toyota");
+        autos.add("Zhiguli");
+        autos.add("Zhiguli");
+        autos.add("Kalina");
         for (String auto : autos) {
             System.out.println(auto);
         }
