ebebc9afb02409314084f202a70869b5dcb50a50
KatUser <ekaterina.shirokikh@gmail.com>
2023-04-21 17:58:46 +0700

    0. Map, HashMap [#212652 #411280]

--- src/main/java/ru/job4j/collection/UsageMapTwo.java
+++ src/main/java/ru/job4j/collection/UsageMapTwo.java
@@ -5,9 +5,14 @@ import java.util.HashMap;
 public class UsageMapTwo {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+        map.put("ppechkin@prostokvashino.ru", "User user");
         map.put("ppechkin@prostokvashino.ru", "Pochtalyon Pechkin");
+        map.put("dfedor@prostokvashino.ru", "Dyadya Vasya");
         map.put("dfedor@prostokvashino.ru", "Dyadya Fedor");
+        map.put("kmatroskin@prostokvashino.ru", "Kot Kot");
         map.put("kmatroskin@prostokvashino.ru", "Kot Matroskin");
+        map.put("psharik@prostokvashino.ru", "Dog dog");
+        map.put("psharik@prostokvashino.ru", "Pios Sharik");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
