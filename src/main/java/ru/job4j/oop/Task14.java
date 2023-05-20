9cfcdc1c7d8d8ee7c141f025fe7cc7cf4242f3de
KatUser <ekaterina.shirokikh@gmail.com>
2023-05-21 06:46:09 +0700

    Task14 & WoodDoll

---
+++ src/main/java/ru/job4j/oop/Task14.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Task14 {
+
+    void method() {
+        class Nested {
+            public static void main(String[] args) {
+                System.out.println("This is inner");
+            }
+        }
+    }
+}
