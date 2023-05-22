69285ca5592eb1fd1aa424dfa8e85af4c66eddcb
KatUser <ekaterina.shirokikh@gmail.com>
2023-05-22 16:24:05 +0700

    2.3. Встроенные функциональные интерфейсы. [#249209] StrategyUsage (with strategy pattern)

--- src/main/java/ru/job4j/function/StrategyUsage.java
+++ src/main/java/ru/job4j/function/StrategyUsage.java
@@ -18,4 +18,23 @@ public class StrategyUsage {
     public boolean check(Predicate<String> pred, String s) {
         return pred.test(s);
     }
+
+    public static void main(String[] args) {
+        StrategyUsage usage = new StrategyUsage();
+        System.out.println(
+                "Результат работы: " + usage.check(
+                        str -> str.isEmpty(), ""
+                )
+        );
+        System.out.println(
+                "Результат работы: " + usage.check(
+                        str -> str.startsWith("Fun"), "Functional interface"
+                )
+        );
+        System.out.println(
+                "Результат работы: " + usage.check(
+                        str -> str.contains("rn"), "Surname Name"
+                )
+        );
+    }
 }
