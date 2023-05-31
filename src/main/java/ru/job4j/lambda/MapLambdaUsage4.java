6da4dece1fbde02483ea1d4c79ffa8b2589cbd01
KatUser <ekaterina.shirokikh@gmail.com>
2023-05-31 19:16:14 +0700

    9. Лямбда выражения и Collection Framework [#505026 # [#505026] - examples

--- src/main/java/ru/job4j/lambda/MapLambdaUsage4.java
+++ src/main/java/ru/job4j/lambda/MapLambdaUsage4.java
@@ -4,15 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
 
-public class MapLambdaUsage3 {
+public class MapLambdaUsage4 {
     public static void main(String[] args) {
         Map<String, Integer> map = new HashMap<>();
         map.put("Shoes", 200);
-        BiFunction<Integer, Integer, Integer> function =
-                (oldValue, newValue) -> oldValue - newValue;
+        BiFunction<Integer, Integer, Integer> function = (oldValue, newValue) -> oldValue - newValue;
         int newPrice = map.merge("Shoes", 50, function);
         System.out.println("New price: " + newPrice);
-
+        System.out.println("Price of shirt: " + map.merge("Shirt", 100, function));
         map.forEach((key, value) -> System.out.println("Key: " + key + ", Value: " + value));
     }
 }
