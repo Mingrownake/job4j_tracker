87999b90da17e5b10ae0cc4c982d7d64d7855b63
KatUser <ekaterina.shirokikh@gmail.com>
2023-06-01 18:27:21 +0700

    9. Лямбда выражения и Collection Framework [#505026 # [#505026] - bestSubject to map.merge

--- src/main/java/ru/job4j/hmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hmap/AnalyzeByMap.java
@@ -65,10 +65,11 @@ public class AnalyzeByMap {
     public static Label bestSubject(List<Pupil> pupils) {
         List<Label> labelList = new LinkedList<>();
         Map<String, Integer> map = new LinkedHashMap<>();
+        BiFunction<Integer, Integer, Integer> function =
+                (oldValue, newValue) -> oldValue + newValue;
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                int score = map.getOrDefault(subject.name(), 0);
-                map.put(subject.name(), score + subject.score());
+                map.merge(subject.name(), subject.score(), function);
             }
         }
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
