d7e32c6c8a3007125f444c7e6ac003697a2d8747
KatUser <ekaterina.shirokikh@gmail.com>
2023-06-12 06:40:56 +0700

    List to Map in streams

--- src/main/java/ru/job4j/stream/ListToMap2.java
+++ src/main/java/ru/job4j/stream/ListToMap2.java
@@ -6,11 +6,11 @@ import java.util.stream.Collectors;
 public class ListToMap2 {
     public static void main(String[] args) {
         System.out.println(
-                List.of(1, 2, 3, 4).stream().collect(
+                List.of(1, 2, 2, 3, 4).stream().collect(
                         Collectors.toMap(
                                 e -> e,
                                 e -> e * e,
-                                (existing, replacement) -> existing
+                                (existing, replacement) -> replacement
                         )
                 )
         );
