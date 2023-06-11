16edce745d8f4d24a9ec217b10c90cf972495c45
KatUser <ekaterina.shirokikh@gmail.com>
2023-06-11 16:49:52 +0700

    List to Map in streams

--- src/main/java/ru/job4j/stream/ListToMap2.java
+++ src/main/java/ru/job4j/stream/ListToMap2.java
@@ -3,7 +3,7 @@ package ru.job4j.stream;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ListToMap {
+public class ListToMap2 {
     public static void main(String[] args) {
         System.out.println(
                 List.of(1, 2, 3, 4).stream().collect(
