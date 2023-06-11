cd254251834d73d47840d7a6ddcd531fd829ea1a
KatUser <ekaterina.shirokikh@gmail.com>
2023-06-11 16:47:13 +0700

    List to Map in streams

---
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListToMap {
+    public static void main(String[] args) {
+        System.out.println(
+                List.of(1, 2, 3, 4).stream().collect(
+                        Collectors.toMap(
+                                e -> e,
+                                e -> e * e,
+                                (existing, replacement) -> existing
+                        )
+                )
+        );
+    }
+}
