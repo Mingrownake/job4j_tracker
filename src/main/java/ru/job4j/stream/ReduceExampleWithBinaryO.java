e4537afdb726e2d6bc5b2d206e59167c3052d65b
KatUser <ekaterina.shirokikh@gmail.com>
2023-06-14 15:54:41 +0700

    Reduce with binaryoperator example

---
+++ src/main/java/ru/job4j/stream/ReduceExampleWithBinaryO.java
@@ -0,0 +1,12 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+public class ReduceExampleWithBinaryO {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4);
+        int sum = nums.stream()
+                .reduce(10, (a, b) -> a + b);
+        System.out.println(sum);
+    }
+}
