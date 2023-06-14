9ffee91049fb1f6ccef485dec6cc476a1d2fc475
KatUser <ekaterina.shirokikh@gmail.com>
2023-06-14 15:51:33 +0700

    moved file

--- src/main/java/ru/job4j/stream/ReduceExample2.java
+++ src/main/java/ru/job4j/stream/ReduceExample2.java
@@ -3,7 +3,7 @@ package ru.job4j.stream;
 import java.util.List;
 import java.util.Optional;
 
-public class ReduceExample {
+public class ReduceExample2 {
     public static void main(String[] args) {
         List<Integer> nums = List.of(1, 2, 3, 4);
         Optional<Integer> sum = nums.stream()
