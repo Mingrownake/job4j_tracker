40c3fef1b331246b7e03f699ed6a44039892ebd2
KatUser <ekaterina.shirokikh@gmail.com>
2023-06-26 06:22:15 +0700

    3. Аттестация [#49777]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -1,7 +1,6 @@
 package ru.job4j.stream;
 
 import java.util.Comparator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -27,7 +26,7 @@ public class Analyze {
     public static List<Tuple> averageScoreBySubject(Stream<Pupil> pupilStream) {
         return pupilStream
                 .flatMap(pupil -> pupil.subjects().stream())
-                .collect(Collectors.groupingBy(Subject::name, LinkedHashMap::new,
+                .collect(Collectors.groupingBy(Subject::name,
                         Collectors.averagingDouble(Subject::score)))
                 .entrySet().stream().map(t -> new Tuple(t.getKey(), t.getValue()))
                 .collect(Collectors.toList());
@@ -45,7 +44,7 @@ public class Analyze {
     public static Tuple bestSubject(Stream<Pupil> pupilStream) {
         return pupilStream
                 .flatMap(pupil -> pupil.subjects().stream())
-                .collect(Collectors.groupingBy(Subject::name, LinkedHashMap::new,
+                .collect(Collectors.groupingBy(Subject::name,
                         Collectors.summingDouble(Subject::score)))
                 .entrySet().stream().map(t -> new Tuple(t.getKey(), t.getValue()))
                 .max(Comparator.comparing(Tuple::score)).orElse(null);
