b898a07cbc626bbd80ec02e55923eeba5578805f
KatUser <ekaterina.shirokikh@gmail.com>
2024-02-10 17:14:16 +0700

    4. Найти утечку памяти. [#504882]

---
+++ src/main/java/ru/job4j/gc/leak/Post.java
@@ -0,0 +1,54 @@
+package ru.job4j.gc.leak;
+
+import java.util.List;
+
+public class Post {
+
+    private Integer id;
+
+    private String text;
+
+    private List<Comment> comments;
+
+    public Post(int id, String text, List<Comment> comments) {
+        this.id = id;
+        this.text = text;
+        this.comments = comments;
+    }
+
+    public Post(String text, List<Comment> comments) {
+        this.text = text;
+        this.comments = comments;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /*
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+    */
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+}
