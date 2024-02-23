84f8c77b0e83dbf835dd533b47217252aea5f65a
KatUser <ekaterina.shirokikh@gmail.com>
2024-02-23 16:05:39 +0700

    2. Принципы Kiss, Dry и Yagni [#238813]

--- src/test/java/ru/job4j/kiss/fool/FoolTest.java
+++ src/test/java/ru/job4j/kiss/fool/FoolTest.java
@@ -3,22 +3,23 @@ package ru.job4j.kiss.fool;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-
 import static org.assertj.core.api.Assertions.*;
 
-class FoolTest {
+public class FoolTest {
 
-   @ParameterizedTest
-   @CsvSource(
-           {
-           "2, 2",
-           "15, FizzBuzz",
-           "3, Fizz",
-           "5, Buzz"
-           }
-   )
-   void whenInputIsCorrect(int startAt, String answer) {
-       Fool fool = new Fool();
-       assertThat(fool.getAnswer(startAt)).isEqualTo(answer);
-   }
+    @ParameterizedTest(name = "{index} - {0} Input is correct")
+    @CsvSource(
+            {
+                    "2, 2",
+                    "15, FizzBuzz",
+                    "3, Fizz",
+                    "5, Buzz"
+            }
+    )
+    void whenInputIsCorrect(int startAt, String answer) {
+        Fool fool = new Fool();
+        assertThat(fool.getAnswer(startAt)).isEqualTo(answer);
+    }
 }
+
+
