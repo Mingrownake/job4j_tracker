1f94351ecd80fd7f1f96f634d99dd2f7c3cd33aa
KatUser <ekaterina.shirokikh@gmail.com>
2024-02-26 08:22:47 +0700

    0. Принцип открытости закрытости [#4914]

---
+++ src/test/java/ru/job4j/ood/ocp/Animal.java
@@ -0,0 +1,27 @@
+package ru.job4j.ood.ocp;
+/*Animal - лучше сделать interface
+* Метод run - лучше сделать без явной реализации
+* Наследование класса Bee от Animal - не корректно, Bee "не является" Animal
+* Поле legs лучше сделать без значения, т.к. наследуется состояние */
+
+public class Animal {
+
+    public int legs = 4;
+
+    public void run() {
+        System.out.println("Running... ");
+    }
+
+    public static void main(String[] args) {
+        Bear bear = new Bear();
+        System.out.println(bear.legs);
+    }
+}
+
+class Bear extends Animal {
+
+}
+
+class Bee extends Animal {
+
+}
